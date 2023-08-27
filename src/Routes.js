0f65a6ff839700542b8cd633db2082a2a11b500d
lucymutunga <lucyalphonce18@gmail.com>
2023-08-27 18:07:32 +0300

    added signup code

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
 
-import Landing from './components/Landing/LandingPage';
-
+import Landing from "./components/Landing/LandingPage";
+import Register from "./components/Register/Register";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Landing />
-    }
-])
-
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/signup",
+    element: <Register />,
+  },
+]);
 
 export default router;
